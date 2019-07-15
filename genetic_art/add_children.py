14eefee24bee2058a9a23f545457c1f98efb5db7
MasonCaiby <maxwell.caudle@gmail.com>
2019-07-14 22:17:48 -0600

    starting to do some mutations with limited number of shapes

--- genetic_art/add_children.py
+++ genetic_art/add_children.py
@@ -1,7 +1,15 @@
-from PIL import Image, ImageDraw, ImageChops
+"""DOCSTRING
+This was my first implementation of genetic programming. It just takes parents and mutates them. The successful
+mutations for each generation create the queue for parents in the next generation as well as the minimum between
+[100, 30% of the number of shapes we have in total].
+
+"""
+
+from PIL import Image, ImageDraw
 import numpy as np
 import random
 import time
+
 
 def load_image(path_to_image):
     true_image = Image.open(path_to_image)
@@ -11,7 +19,7 @@ def load_image(path_to_image):
 
 
 def show_two_pics(pic1, pic2):
-    Image.fromarray(np.hstack((np.array(pic1),np.array(pic2)))).show()
+    Image.fromarray(np.hstack((np.array(pic1), np.array(pic2)))).show()
 
 
 class NewImage():
@@ -25,8 +33,8 @@ class NewImage():
         self.image = None
 
         self.rand_var = lambda: random.uniform(-max_variance, max_variance)
-        self.rand_height = lambda: self.rand_var()*height
-        self.rand_width = lambda : self.rand_var()*width
+        self.rand_height = lambda: self.rand_var() * height
+        self.rand_width = lambda: self.rand_var() * width
 
         self.shapes = []
         self.latest_gen = []
@@ -58,9 +66,8 @@ class NewImage():
                           int(point[1] + self.rand_height()))
                          for point in shape[0]]
 
-            new_color = tuple(int(color+color*self.rand_var())
-                              for color in shape[1][:3])\
-                        +(125,)
+            new_color = tuple(int(color + color * self.rand_var()) for color in shape[1][:3])
+            new_color += (125,)
 
             new_shapes.append((new_shape, new_color))
         return new_shapes
@@ -69,7 +76,7 @@ class NewImage():
         made_shapes = []
         shapes_to_mutate = self.latest_gen + \
                            random.choices(self.shapes,
-                                          k=min(100, int(0.3*len(self.shapes))))
+                                          k=min(100, int(0.3 * len(self.shapes))))
         start = time.time()
         for i, shape in enumerate(shapes_to_mutate):
             new_shapes = self.mutate_shape(shape)
@@ -78,20 +85,18 @@ class NewImage():
                 draw = ImageDraw.Draw(new_image, 'RGBA')
                 draw.polygon(new_shape[0], new_shape[1])
                 if self.compare_new_images(new_image):
-                    print("{:.2f}% done".format(100*i/len(shapes_to_mutate)))
+                    print("{:.2f}% done".format(100 * i / len(shapes_to_mutate)))
                     made_shapes.append(new_shape)
 
-        print("\n"*2,
-              "time per image:", (time.time()-start)/(5*len(shapes_to_mutate)),
+        print("\n" * 2,
+              "time per image:", (time.time() - start) / (5 * len(shapes_to_mutate)),
               "number of shapes:", len(self.shapes),
-              "\n"*2)
+              "\n" * 2)
         self.shapes += self.latest_gen
         self.latest_gen = made_shapes
 
     def compare_new_images(self, new_image):
-        new_diff = sum(np.fabs(np.subtract(
-                                       self.true_image.getdata(),
-                                       new_image.getdata())).flatten())
+        new_diff = sum(np.fabs(np.subtract(self.true_image.getdata(), new_image.getdata())).flatten())
 
         if new_diff < self.difference:
             self.image = new_image
@@ -100,15 +105,17 @@ class NewImage():
             return True
         return False
 
+
 if __name__ == "__main__":
     true_image, data, width, height = load_image("source_images/mona_lisa.jpg")
     new_image = NewImage(true_image, width, height)
     new_image.make_new_image()
-    new_image.init_shapes(num_shapes=1000, sides=3)
+    new_image.init_shapes(num_shapes=3000, sides=3)
     new_image.image.show()
+    i = 0
     while new_image.difference > 108810:
         new_image.make_new_shapes()
-        new_image.image.show()
+        i += 1
+        if not i % 100:
+            new_image.image.show()
     new_image.image.show()
-
-
---
+++ genetic_art/alter_shapes.py
@@ -0,0 +1,129 @@
+""" DOCSTRING
+In this file I'll try to change each of 50 shapes to see if I can improve make a picture fit the original.
+
+https://rogerjohansson.blog/2008/12/07/genetic-programming-evolution-of-mona-lisa/
+
+I guess the idea is to pop a shape off the list of shapes, alter it a handful of times, and keep the shape that has the
+best information gain.
+"""
+
+from PIL import Image, ImageDraw
+import numpy as np
+import random
+import time
+
+
+def load_image(path_to_image):
+    true_image = Image.open(path_to_image)
+    data = true_image.getdata()
+    width, height = true_image.size
+    return true_image, data, width, height
+
+
+def show_two_pics(pic1, pic2):
+    Image.fromarray(np.hstack((np.array(pic1), np.array(pic2)))).show()
+
+class NewImage:
+    def __init__(self, true_image, width, height, num_sides=3, num_shapes=50, max_variance=0.1, num_children=5):
+        self.true_image = true_image
+        self.width = width
+        self.height = height
+        self.num_sides = num_sides
+        self.num_shapes = num_shapes
+        self.max_variance = max_variance
+        self.num_children = num_children
+
+        self.background = Image.new('RGB', (self.width, self.height), color=0)
+        self.shapes = []
+
+    def init_shapes(self):
+        for _ in range(self.num_shapes):
+            self.shapes.append(Shape(self.num_sides, self.height, self.width, self.num_children, self.max_variance))
+
+    def display_image(self):
+        image = self.draw_shapes()
+        image.show()
+
+    def draw_shapes(self):
+        image = self.background.copy()
+        draw = ImageDraw.Draw(image, 'RGBA')
+        for shape in self.shapes:
+            draw.polygon(shape.vertices, shape.color)
+        return image
+
+    def one_generation(self):
+        start = time.time()
+        for i in range(len(self.shapes)):
+            shape = self.shapes.pop(i)
+            image = self.draw_shapes()
+            shape = self.check_mutants(shape, image)
+            self.shapes.insert(i, shape)
+        print("Time for one generation:", (time.time()-start)/(self.num_shapes*self.num_children))
+
+    def check_mutants(self, shape, image):
+        mutants = shape.mutate_shape()
+        best_diff = float('INF')
+
+        for mutant in mutants:
+            check_image = image.copy()
+            draw = ImageDraw.Draw(check_image, 'RGBA')
+            draw.polygon(mutant[0], mutant[1])
+            new_diff = sum(np.fabs(np.subtract(self.true_image.getdata(), check_image.getdata())).flatten())
+            if new_diff < best_diff:
+                best_diff = new_diff
+                best_mutant = mutant
+
+        shape.vertices = best_mutant[0]
+        shape.color = best_mutant[1]
+
+        return shape
+
+
+
+
+class Shape:
+    def __init__(self, num_sides, height, width, num_children, max_variance):
+        self.num_sides = num_sides
+        self.height = height
+        self.width = width
+        self.num_children = num_children
+        self.max_variance = max_variance
+
+        self.vertices = None
+        self.color = None
+
+        self.rand_var = lambda: random.uniform(-max_variance, max_variance)
+        self.rand_height = lambda: self.rand_var() * height
+        self.rand_width = lambda: self.rand_var() * width
+
+        self.init_shape()
+
+    def init_shape(self):
+        self.vertices = [(random.randint(0, self.width),
+                  random.randint(0, self.height)) for _ in range(self.num_sides)]
+
+        self.color = tuple(random.randint(0, 255) for _ in range(3)) + (125,)
+
+    def mutate_shape(self):
+        children = [(self.vertices, self.color)]
+        for _ in range(self.num_children):
+
+            vertices = [(int(point[0] + self.rand_width()),
+                         int(point[1] + self.rand_height()))
+                        for point in self.vertices]
+            color = tuple(int(color + color * self.rand_var()) for color in self.color[:3])
+            color += (125,)
+
+            children.append((vertices, color))
+        return children
+
+if __name__ == "__main__":
+    true_image, data, width, height = load_image("source_images/mona_lisa.jpg")
+    new_image = NewImage(true_image, width, height)
+    new_image.init_shapes()
+    new_image.display_image()
+    for _ in range(1000):
+        new_image.one_generation()
+        new_image.display_image()
+
+
